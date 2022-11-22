ec9fed77af309e6df69d78d80ece7bd2f2bba27d
Alex Marchant <alex_marchant@outlook.com>
2022-11-23 00:28:10 +0100

    Add InvoiceItem class

---
+++ InvoiceGenerator/Models/InvoiceItem.swift
@@ -0,0 +1,32 @@
+//
+//  InvoiceItem.swift
+//  InvoiceGenerator
+//
+//  Created by Alex Marchant on 23/11/2022.
+//
+
+import Foundation
+
+class InvoiceItem: ObservableObject {
+    
+    @Published var quantity: Int
+    @Published var name: String
+    @Published var description: String?
+    @Published var price: Double
+    
+    var totalPrice: Double {
+        return price * Double(quantity)
+    }
+    
+    init(
+        quantity: Int,
+        name: String,
+        price: Double,
+        description: String? = nil
+    ) {
+        self.quantity = quantity
+        self.name = name
+        self.price = price
+        self.description = description
+    }
+}
