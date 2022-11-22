577cf06094ac9bf40a7d570e511641630db16f99
Alex Marchant <alex_marchant@outlook.com>
2022-11-22 20:47:57 +0100

    Initial project

---
+++ InvoiceGenerator/InvoiceGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceGeneratorApp.swift
+//  InvoiceGenerator
+//
+//  Created by Alex Marchant on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
